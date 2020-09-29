fe253cef2cf4362b426f6a8d01f88fe47bb4d081
zcg812 <zcg812@163.com>
2020-09-29 20:48:55 +0800

    Initial commit

---
+++ admin/foot.php
@@ -0,0 +1,5 @@
+    </div>
+  </div>
+  <div id="footer"><div id="footer_box"><span>Powered by <a href="http://www.kszhch.com/" target="_blank">SiNianBlogMi</a></span></div></div>
+</body>
+</html>
---
+++ files/conf.php
@@ -0,0 +1,11 @@
+﻿<?php
+$mc_config = array (
+  'site_link' => 'http://127.0.0.1',
+  'site_name' => '思念博客迷',
+  'site_desc' => '基于php迷你博客程序',
+  'user_name' => 'admin',
+  'user_pass' => 'admin',
+  'user_nick' => 'SiNnianBlogMi',
+  'comment_code' => '',
+)
+?>
